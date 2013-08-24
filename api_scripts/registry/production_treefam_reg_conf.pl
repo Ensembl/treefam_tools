d83c2c20fc2d6035b97e9052e339dcd695783bbf
Fabian Schreiber <fs@ebi-003.ebi.ac.uk>
2013-08-24 23:41:54 +0100

    fixed wrong db server address

--- api_scripts/registry/production_treefam_reg_conf.pl
+++ api_scripts/registry/production_treefam_reg_conf.pl
@@ -14,38 +14,7 @@ use Bio::EnsEMBL::DBSQL::DBAdaptor;
 use Bio::EnsEMBL::Compara::DBSQL::DBAdaptor;
 
 
-# The majority of core databases live on two staging servers:
-#Bio::EnsEMBL::Registry->load_registry_from_url('mysql://ensro@ens-livemirror/67');
-#Bio::EnsEMBL::Registry->load_registry_from_url('mysql://anonymous@mysql.ebi.ac.uk:4157');
-
-#Bio::EnsEMBL::Registry->load_registry_from_url(
-  #'mysql://ensro@ens-staging1/68');
-
-  #Bio::EnsEMBL::Registry->load_registry_from_url(
-    #'mysql://ensro@ens-staging2/68');
-
-#Bio::EnsEMBL::Registry->load_registry_from_db(
-#    -host => 'mysql.ebi.ac.uk',
-#        -port => 4157,
-#            -user => 'anonymous',
-#                -verbose => 1 );
-
-#Bio::EnsEMBL::Registry->load_registry_from_multiple_dbs(
-    #{
-            #-host => 'mysql.ebi.ac.uk',
-                #-port => 4157,
-                    #-user => 'anonymous',
-                        #-verbose => 1,
-            #-db_version => 68, 
-   #},
-    #{
-            #-host => 'ens-livemirror',
-            #-user => 'ensro',
-            #-port => '3306',
-            #-verbose => 1
-    #}
-    #);
-    
+   
 # Extra core databases that live on genebuilders' servers:
 
 # Compara databases used during the release (master, previous and current compara dbs)
@@ -53,29 +22,11 @@ use Bio::EnsEMBL::Compara::DBSQL::DBAdaptor;
 
 # Master
 Bio::EnsEMBL::Compara::DBSQL::DBAdaptor->new(
-    -host => 'mysql-treefam-public',
+    -host => 'mysql-treefam-public.ebi.ac.uk',
     -user => 'treefam_ro',
     -pass => '',
     -port => 4418,
     -species => 'TreeFam',
     -dbname => 'treefam_production_9_69',
-    #-dbname => 'treefam_homology_67hmm',
 );
-#Bio::EnsEMBL::Compara::DBSQL::DBAdaptor->new(
-    #-host => 'web-mei-treefam',
-    #-user => 'treefam_admin',
-    #-pass => 'treefam_king1982',
-    #-port => 3365,
-    #-species => 'compara_master',
-    #-dbname => 'treefam_master9',
-#);
-# Prev release
-#Bio::EnsEMBL::Compara::DBSQL::DBAdaptor->new(
-    #-host => 'mysql.ebi.ac.uk',
-    #-user => 'anonymous',
-    #-port => 4157,
-    #-species => 'compara_prev',
-    #-dbname => 'ensembl_compara_68',
-#);
-
 1;
